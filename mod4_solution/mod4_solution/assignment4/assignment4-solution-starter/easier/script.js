af7e97361ee3cf20cc5aa078682b7b297bcbbcb2
Lucia Barrenechea <lbarrenecheac@gmail.com>
2023-04-16 18:23:36 -0600

    corrected module 4 archives

--- mod4_solution/mod4_solution/assignment4/assignment4-solution-starter/easier/script.js
+++ mod4_solution/mod4_solution/assignment4/assignment4-solution-starter/easier/script.js
@@ -65,3 +65,37 @@ for (var i = 0; i < names.length; i++) {
 }
 
 })();
+
+(function (window) {
+  var speakWord = "Good Bye";
+  var byeSpeaker = {
+    speak: function (name) {
+      console.log(speakWord + " " + name);
+    }
+  };
+  window.byeSpeaker = byeSpeaker;
+})(window);
+
+(function (window) {
+  var speakWord = "Hello";
+  var helloSpeaker = {
+    speak: function (name) {
+      console.log(speakWord + " " + name);
+    }
+  };
+  window.helloSpeaker = helloSpeaker;
+})(window);
+
+(function (window) {
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+  for (var i in names) {
+    var firstLetter = names[i].charAt(0).toLowerCase();
+    if (firstLetter === 'j') {
+      byeSpeaker.speak(names[i]);
+    } else {
+      helloSpeaker.speak(names[i]);
+    }
+  }
+})(window);
+
